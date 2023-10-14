959ead5825a93eabfd7d08f73683871fb00f3444
Ngai Tik Chung <NgaitikK.Chung94@gmail.com>
2023-10-13 22:29:10 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    }
+  
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    }       
+  
+function overwriteBestCustomer(newName) {
+    bestCustomer = 'maybe bob'
+    }
+
+const leastFavoriteCustomer = 'B';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'OB';
+}
+
+changeLeastFavoriteCustomer();
